a822877f219c70a86b0f13fef76a555ffc0970e8
sajumulakkal <sajumulakkal@hotmail.com>
2026-07-31 08:40:42 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ const configureRoutes = (app) => {
   app.use('/api/users', require('./api/users'));
   app.use('/api/chips', require('./api/chips'));
   app.use('/api/players', require('./api/players'));
-  app.use('/', (req, res) => {
+
+  // Exact root match only
+  app.get('/', (req, res) => {
     res.status(200).send('GGLab API Documents');
+  });
+
+  // Catch-all 404 handler
+  app.use((req, res) => {
+    res.status(404).json({ error: 'Endpoint not found' });
   });
 };
 
-module.exports = configureRoutes;  
+module.exports = configureRoutes;
